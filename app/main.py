372bf2bc8545d48885a39d93bf1a7e12df361f97
sla-tur <sla.tur1999@gmail.com>
2025-01-13 00:03:47 +0100

    working on repl

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import sys
 
-
 def main():
     # Uncomment this block to pass the first stage
+    commands = {exit}
+
     sys.stdout.write("$ ")
     cmd = input()
-    while cmd:
+    while cmd not in commands:
         print(f"{cmd}: command not found")
         sys.stdout.write("$ ")
         cmd = input()
+    if cmd[0:3] == 'exit':
+        return 0
 
 if __name__ == "__main__":
     main()
