e8e1b234beb1cde568ada7bb13921c5be164854a
sla-tur <sla.tur1999@gmail.com>
2025-01-12 23:57:08 +0100

    repl start

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-    #
     cmd = input()
-    print(f"{cmd}: command not found")
+    while cmd:
+        print(f"{cmd}: command not found")
+        cmd = input()
 
 if __name__ == "__main__":
     main()
