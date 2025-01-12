db31cdd5bc1aff2f356a903a603236d8df184237
sla-tur <sla.tur1999@gmail.com>
2025-01-12 23:51:58 +0100

    added invalid command response

--- app/main.py
+++ app/main.py
@@ -5,9 +5,8 @@ def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    input()
-
+    cmd = input()
+    print(f"<{cmd}>: command not found")
 
 if __name__ == "__main__":
     main()
