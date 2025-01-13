c296f0dc5e695cde0d56a1e1a18e9d0d425bfa70
sla-tur <sla.tur1999@gmail.com>
2025-01-13 03:49:43 +0100

    working on repl

--- app/main.py
+++ app/main.py
@@ -2,16 +2,31 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    commands = {"exit 0"}
+    commands = {"exit", "echo"}
     
     while True:
         sys.stdout.write("$ ")
         cmd = input()
-        if cmd not in commands:
-            print(f"{cmd}: command not found")
-            continue
-        else:
-            return 0
+        args = cmd.split()
+        try:
+            if args[0] not in commands:
+                print(f"{args[0]}: command not found")
+                continue
+            else:
+                if args[0] == 'exit':
+                    try:
+                        return int(args[1])
+                    except:
+                        print(f"{cmd}: command not found")
+                        continue
+                elif args[0] == 'echo':
+                    print(" ".join(args[1:]))
+        except:
+            if args[0] not in commands:
+                print(f"{args[0]}: command not found")
+                continue
+            else:
+                return 0
 
 if __name__ == "__main__":
     main()
