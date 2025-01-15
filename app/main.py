426496c3805833869ecb4f5e0711d76ae11bf4e8
sla-tur <sla.tur1999@gmail.com>
2025-01-15 00:18:00 +0000

    working on executables

--- app/main.py
+++ app/main.py
@@ -7,31 +7,24 @@ def main():
     
     while True:
         sys.stdout.write("$ ")
-        cmd = input()
+        sys.stdout.flush()
+        # readline() here ends with a newline character, necessitating rstrip()
+        cmd = sys.stdin.readline().rstrip()
+        # Split input into distinct arguments
         args = cmd.split()
-        try:
-            if args[0] not in commands:
-                print(f"{args[0]}: command not found")
-                continue
-            else:
-                match args:
-                    case ["exit", "0"]:
-                        exit()
-                    case ["echo", *pos_args]:
-                        print(*pos_args)
-                    case ["type", command]:
-                        if command in commands:
-                            print(f"{command} is a shell builtin")
-                        else:
-                            print(f"{command}: not found")
-                    case _:
-                        print(f"{cmd}: command not found")
-        except:
-            if args[0] not in commands:
-                print(f"{args[0]}: command not found")
-                continue
-            else:
-                return 0
+        match args:
+            # the exit command only exits on receipt of a valid exit code -- 0
+            case ["exit", "0"]:
+                exit()
+            case ["echo", *pos_args]:
+                print(*pos_args)
+            case ["type", command]:
+                if command in commands:
+                    print(f"{command} is a shell builtin")
+                else:
+                    print(f"{command}: not found")
+            case _:
+                print(f"{cmd}: command not found")
 
 if __name__ == "__main__":
     main()
