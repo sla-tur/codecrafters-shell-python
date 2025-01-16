4ef0ae93543307f8cdd6db4dd046df02d3cea669
sla-tur <sla.tur1999@gmail.com>
2025-01-16 13:35:18 +0000

    finishing work on executables

--- app/main.py
+++ app/main.py
@@ -2,38 +2,42 @@ import sys
 import os
 import subprocess
 
+PATH = os.environ.get("PATH")
+paths = PATH.split(os.pathsep)
+commands = {"exit", "echo", "type"}
+
+def echo(*args):
+    print(*args)
+
+def type(command):
+    command_path = None
+    for path in paths:
+        if os.path.isfile(f"{path}/{command}"):
+            command_path = f"{path}/{command}"
+    if command in commands:
+        sys.stdout.write(f"{command} is a shell builtin\n")
+    elif command_path:
+        sys.stdout.write(f"{command} is {command_path}\n")
+    else:
+        sys.stdout.write(f"{command}: not found\n")
+    sys.stdout.flush()
+
 def main():
-    # Uncomment this block to pass the first stage
-    PATH = os.environ.get("PATH")
-    commands = {"exit", "echo", "type"}
-    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # readline() here ends with a newline character, necessitating rstrip()
         cmd = sys.stdin.readline().rstrip()
-        # Split input into distinct arguments
+        # split input into distinct arguments
         args = cmd.split()
         match args:
-            # the exit command only exits on receipt of a valid exit code -- 0
+            # the exit command defaults to a 0 code
             case ["exit", "0"]:
-                exit()
+                sys.exit(int(args[1]) if len(args) > 1 else 0)
             case ["echo", *pos_args]:
-                #
-                print(*pos_args)
+                echo(*pos_args)
             case ["type", command]:
-                paths = PATH.split(os.pathsep)
-                command_path = None
-                for path in paths:
-                    if os.path.isfile(f"{path}/{command}"):
-                        command_path = f"{path}/{command}"
-                if command in commands:
-                    sys.stdout.write(f"{command} is a shell builtin\n")
-                elif command_path:
-                    sys.stdout.write(f"{command} is {command_path}\n")
-                else:
-                    sys.stdout.write(f"{command}: not found\n")
-                sys.stdout.flush()
+                type(command)
             case _:
                 paths = PATH.split(os.pathsep)
                 command_path = None
@@ -41,8 +45,7 @@ def main():
                     if os.path.isfile(f"{path}/{args[0]}"):
                         command_path = f"{path}/{args[0]}"
                 if command_path:
-                    print(subprocess.run(args, capture_output=True,
-                                         text=True).stdout)
+                    subprocess.run(args)
                 else:
                     sys.stdout.write(f"{args[0]}: not found\n")
                 sys.stdout.flush()
